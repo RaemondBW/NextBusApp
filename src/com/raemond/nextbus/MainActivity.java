24eb603e060eeaec8b9ab5269dadd9cdc6026127
Raemond Bergstrom-Wood <raemond.bw@gmail.com>
2013-03-27 12:25:25 -0700

    Minor modifications. Need to figure out why the dialog is broken.

--- src/com/raemond/nextbus/MainActivity.java
+++ src/com/raemond/nextbus/MainActivity.java
@@ -65,6 +65,8 @@ public class MainActivity extends Activity {
 	static HashMap<String,String> routemap;
 	static ArrayList <String> agencies = new ArrayList<String>();
 	static ArrayList <String> routes = new ArrayList<String>();
+	static ArrayList <String> directions = new ArrayList<String>();
+	static ArrayList <String> stopList = new ArrayList<String>();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -204,13 +206,26 @@ public class MainActivity extends Activity {
 			routespinner.setOnItemSelectedListener(new routelistener());
     		
     		
-    		/*final LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    		LinearLayout linearLayout = (LinearLayout)findViewById(R.id.listOfStops);
-    		FrameLayout temp = (FrameLayout) inflater.inflate(R.layout.bus_info_fragment,null);
-    		linearLayout.addView(temp);
-    		
-    		TextView stops = (TextView) temp.findViewById(R.id.bus_stop);
-    		Bus_Stop new_stop = new Bus_Stop(agency,"AC Transit", route, "Hearst Av & Le Roy Av", stop, temp);
+			directionspinner = (Spinner) dialog.findViewById(R.id.directionSpinner);
+			ArrayAdapter<String> directionArrayAdapter = new ArrayAdapter<String>(MainActivity.this,
+					android.R.layout.simple_expandable_list_item_1,directions);//getRoutes(agency));
+			directionspinner.setAdapter(directionArrayAdapter);
+			directionspinner.setOnItemSelectedListener(new directionlistener());
+			
+			
+			stopspinner = (Spinner) dialog.findViewById(R.id.stopSpinner);
+			ArrayAdapter<String> stopArrayAdapter = new ArrayAdapter<String>(MainActivity.this,
+					android.R.layout.simple_expandable_list_item_1,stopList);
+			stopspinner.setAdapter(stopArrayAdapter);
+			stopspinner.setOnItemSelectedListener(new stoplistener());
+			
+			
+			/*final LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			LinearLayout linearLayout = (LinearLayout)findViewById(R.id.listOfStops);
+			FrameLayout temp = (FrameLayout) inflater.inflate(R.layout.bus_info_fragment,null);
+			linearLayout.addView(temp);
+			
+			Bus_Stop new_stop = new Bus_Stop(agency,"AC Transit", route, "Hearst Av & Le Roy Av", stop, temp);
 			stops.add(new_stop);*/
         	
     		break;
@@ -225,34 +240,7 @@ public class MainActivity extends Activity {
     	Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
 	
-	/*private static String getPrediction(String agency, String route, String stop) {//Add check for no current prediction
-		URL url;
-		URLConnection connection;
-		DocumentBuilder dBuilder;
-		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-		String time = new String();
-		Log.v("function","called");
-		try {
-			url = new URL("http://webservices.nextbus.com/service/publicXMLFeed?command=predictions&a=" + agency + "&stopId=" + stop + "&routeTag=" + route);
-			connection = url.openConnection();
-			dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.parse(connection.getInputStream());
-			
-			NodeList nList = doc.getElementsByTagName("prediction");
-			System.out.println("Prediction for " + agency + " " + route + " " + stop);
-			for (int i=0; i<nList.getLength(); i++){
-				Node nNode = nList.item(i);
-				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-					Element eElement = (Element) nNode;
-					Log.v("Bus Arrival Prediction", eElement.getAttribute("minutes") + " minutes");
-					return eElement.getAttribute("minutes");
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return "None";
-	}*/
+	
     class RetrieveAgencies extends AsyncTask<String, Void, String[]> {
 		protected String[] doInBackground(String... urls){
 			//ArrayList<String> agencies = new ArrayList<String>();
@@ -260,6 +248,7 @@ public class MainActivity extends Activity {
 			URLConnection connection;
 			DocumentBuilder dBuilder;
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			agencies.clear();
 			agencymap = new HashMap<String,String>();
 			try {
 				url = new URL("http://webservices.nextbus.com/service/publicXMLFeed?command=agencyList");
@@ -293,36 +282,6 @@ public class MainActivity extends Activity {
 
 	}
 	
-	/*private static ArrayList<String> getAgencies() {
-    //private static String[] getAgencies() {
-		ArrayList<String> agencies = new ArrayList<String>();
-		URL url;
-		URLConnection connection;
-		DocumentBuilder dBuilder;
-		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-		agencymap = new HashMap<String,String>();
-		try {
-			url = new URL("http://webservices.nextbus.com/service/publicXMLFeed?command=agencyList");
-			connection = url.openConnection();
-			dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.parse(connection.getInputStream());
-			
-			NodeList nList = doc.getElementsByTagName("agency");
-			
-			for (int i=0; i<nList.getLength(); i++){
-				Node nNode = nList.item(i);
-				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-					Element eElement = (Element) nNode;
-					agencies.add(eElement.getAttribute("title"));
-					agencymap.put(eElement.getAttribute("title"),eElement.getAttribute("tag"));
-				}
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return agencies;
-		//return (String[]) agencies.toArray(new String[0]);
-	}*/
 	
     class RetrieveRoutes extends AsyncTask<String, Void, String[]> {
 		protected String[] doInBackground(String... urls){
@@ -331,6 +290,7 @@ public class MainActivity extends Activity {
 			URLConnection connection;
 			DocumentBuilder dBuilder;
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			routes.clear();
 			try {
 				url = new URL("http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a="+agency);
 				connection = url.openConnection();
@@ -362,34 +322,52 @@ public class MainActivity extends Activity {
 
 	}
     
-	/*public static ArrayList<String> getRoutes(String agency) {
-		ArrayList<String> routes = new ArrayList<String>();
-		Log.v("Get routes", "called");
-		URL url;
-		URLConnection connection;
-		DocumentBuilder dBuilder;
-		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-		try {
-			url = new URL("http://webservices.nextbus.com/service/publicXMLFeed?command=routeList&a="+agency);
-			connection = url.openConnection();
-			dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.parse(connection.getInputStream());
-			
-			NodeList nList = doc.getElementsByTagName("route");
-			
-			for (int i=0; i<nList.getLength(); i++){
-				Node nNode = nList.item(i);
-				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-					Element eElement = (Element) nNode;
-					Log.v("Route", eElement.getAttribute("title"));
-					routes.add(eElement.getAttribute("title"));
+    
+    class RetrieveDirections extends AsyncTask<String, Void, String[]> {
+		protected String[] doInBackground(String... urls){
+			//ArrayList<String> directions = new ArrayList<String>();
+			URL url;
+			URLConnection connection;
+			DocumentBuilder dBuilder;
+			//boolean useForUI = false;
+			directions.clear();
+			directionmap = new HashMap<String,String>();
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			try {
+				url = new URL("http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a="+agency+"&r="+route);
+				connection = url.openConnection();
+				dBuilder = dbFactory.newDocumentBuilder();
+				Document doc = dBuilder.parse(connection.getInputStream());
+				
+				NodeList nList = doc.getElementsByTagName("direction");
+				
+				for (int i=0; i<nList.getLength(); i++){
+					Node nNode = nList.item(i);
+					if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+						Element eElement = (Element) nNode;
+						if (!"".equals(eElement.getAttribute("title")) && eElement.getAttribute("useForUI").equals("true")){
+							//useForUI = true;
+							Log.v("Direction", eElement.getAttribute("title"));
+							directionmap.put(eElement.getAttribute("title"), eElement.getAttribute("tag"));
+							directions.add(eElement.getAttribute("title"));
+						}
+					}
 				}
+				
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
+			//return directions;
+			return null;
 		}
-		return routes;
-	}*/
+
+		protected void onPostExecute(String[] result) {
+			//Log.v("Bus Prediction",result);
+			//TextView estimation = (TextView) findViewById(R.id.time);
+			//estimation.setText(result);
+		}
+
+	}
 	
 	public static ArrayList<String> getDirection(String agency, String route) {//changed type from bool to arraylist
 		ArrayList<String> directions = new ArrayList<String>();
@@ -426,59 +404,71 @@ public class MainActivity extends Activity {
 		return directions;
 	}
 	
-	public static ArrayList<String> getStops(String agency, String route, String direction) {
-		HashMap<String,String> stops = new HashMap<String,String>();//tag and stop Name
-		HashMap<String,String> ids = new HashMap<String,String>();//tag and stop id
-		ArrayList<String> returnStops = new ArrayList<String>();
-		URL url;
-		URLConnection connection;
-		DocumentBuilder dBuilder;
-		DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-		Log.v("Direction",direction);
-		try {
-			url = new URL("http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a="+agency+"&r="+route);
-			connection = url.openConnection();
-			dBuilder = dbFactory.newDocumentBuilder();
-			Document doc = dBuilder.parse(connection.getInputStream());
-			
-			NodeList nList = doc.getElementsByTagName("stop");
-			
-			for (int i=0; i<nList.getLength(); i++){
-				Node nNode = nList.item(i);
-				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-					Element eElement = (Element) nNode;
-					if (!"".equals(eElement.getAttribute("title"))){
-						stops.put(eElement.getAttribute("tag"), eElement.getAttribute("title"));
-						ids.put(eElement.getAttribute("tag"), eElement.getAttribute("stopId"));
+	
+	class RetrieveStops extends AsyncTask<String, Void, String[]> {
+		protected String[] doInBackground(String... urls){
+			HashMap<String,String> m_stops = new HashMap<String,String>();//tag and stop Name
+			HashMap<String,String> ids = new HashMap<String,String>();//tag and stop id
+			//ArrayList<String> returnStops = new ArrayList<String>();
+			URL url;
+			URLConnection connection;
+			DocumentBuilder dBuilder;
+			DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+			//Log.v("Direction",direction);
+			stopList.clear();
+			try {
+				url = new URL("http://webservices.nextbus.com/service/publicXMLFeed?command=routeConfig&a="+agency+"&r="+route);
+				connection = url.openConnection();
+				dBuilder = dbFactory.newDocumentBuilder();
+				Document doc = dBuilder.parse(connection.getInputStream());
+				
+				NodeList nList = doc.getElementsByTagName("stop");
+				
+				for (int i=0; i<nList.getLength(); i++){
+					Node nNode = nList.item(i);
+					if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+						Element eElement = (Element) nNode;
+						if (!"".equals(eElement.getAttribute("title"))){
+							m_stops.put(eElement.getAttribute("tag"), eElement.getAttribute("title"));
+							ids.put(eElement.getAttribute("tag"), eElement.getAttribute("stopId"));
+						}
 					}
+					
 				}
 				
-			}
-			
-			nList = doc.getElementsByTagName("direction");
-			for (int i=0; i<nList.getLength(); i++) {
-				Node nNode = nList.item(i);
-				if (nNode.getNodeType() == Node.ELEMENT_NODE) {
-					Element eElement = (Element) nNode;
-					if (eElement.getAttribute("tag").equals(direction)) {
-						NodeList stop = nNode.getChildNodes();
-						Log.v("Length",Integer.toString(stop.getLength()));
-						for (int n=0; n<stop.getLength(); n++) {
-							Node stopData = stop.item(n);
-							if (stopData.getNodeType() == Node.ELEMENT_NODE) {
-								Element individualStop = (Element) stopData;
-								Log.v(stops.get(individualStop.getAttribute("tag")),ids.get(individualStop.getAttribute("tag")));
-								returnStops.add(stops.get(individualStop.getAttribute("tag")));
-								routemap.put(stops.get(individualStop.getAttribute("tag")), ids.get(individualStop.getAttribute("tag")));
+				nList = doc.getElementsByTagName("direction");
+				for (int i=0; i<nList.getLength(); i++) {
+					Node nNode = nList.item(i);
+					if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+						Element eElement = (Element) nNode;
+						if (eElement.getAttribute("tag").equals(direction)) {
+							NodeList stop = nNode.getChildNodes();
+							Log.v("Length",Integer.toString(stop.getLength()));
+							for (int n=0; n<stop.getLength(); n++) {
+								Node stopData = stop.item(n);
+								if (stopData.getNodeType() == Node.ELEMENT_NODE) {
+									Element individualStop = (Element) stopData;
+									Log.v(m_stops.get(individualStop.getAttribute("tag")),ids.get(individualStop.getAttribute("tag")));
+									stopList.add(m_stops.get(individualStop.getAttribute("tag")));
+									routemap.put(m_stops.get(individualStop.getAttribute("tag")), ids.get(individualStop.getAttribute("tag")));
+								}
 							}
 						}
 					}
 				}
+			} catch (Exception e) {
+				e.printStackTrace();
 			}
-		} catch (Exception e) {
-			e.printStackTrace();
+			//return returnStops;
+			return null;
 		}
-		return returnStops;
+
+		protected void onPostExecute(String[] result) {
+			//Log.v("Bus Prediction",result);
+			//TextView estimation = (TextView) findViewById(R.id.time);
+			//estimation.setText(result);
+		}
+
 	}
 	
 	public class agencylistener implements OnItemSelectedListener {
@@ -510,12 +500,19 @@ public class MainActivity extends Activity {
 		@Override
 		public void onItemSelected(AdapterView<?> parent, View arg1, int pos,
 				long id) {
-			directionspinner = (Spinner)findViewById(R.id.directionSpinner);
+			//directionspinner = (Spinner)findViewById(R.id.directionSpinner);
 			route = parent.getItemAtPosition(pos).toString();
-			ArrayAdapter<String> directionArrayAdapter = new ArrayAdapter<String>(MainActivity.this,
+			AsyncTask<String, Void, String[]> task = new RetrieveDirections().execute(" ");
+			try {
+				task.get(1000, TimeUnit.MILLISECONDS);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			/*ArrayAdapter<String> directionArrayAdapter = new ArrayAdapter<String>(MainActivity.this,
 					android.R.layout.simple_expandable_list_item_1,getDirection(agency,route));
 			directionspinner.setAdapter(directionArrayAdapter);
-			directionspinner.setOnItemSelectedListener(new directionlistener());
+			directionspinner.setOnItemSelectedListener(new directionlistener());*/
 		}
 
 		@Override
@@ -528,12 +525,47 @@ public class MainActivity extends Activity {
 		@Override
 		public void onItemSelected(AdapterView<?> parent, View arg1, int pos,
 				long id) {
-			stopspinner = (Spinner)findViewById(R.id.stopSpinner);
+			//stopspinner = (Spinner)findViewById(R.id.stopSpinner);
 			direction = directionmap.get(parent.getItemAtPosition(pos).toString());
-			ArrayAdapter<String> stopArrayAdapter = new ArrayAdapter<String>(MainActivity.this,
+			AsyncTask<String, Void, String[]> task = new RetrieveStops().execute(" ");
+			try {
+				task.get(1000, TimeUnit.MILLISECONDS);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			/*ArrayAdapter<String> stopArrayAdapter = new ArrayAdapter<String>(MainActivity.this,
 					android.R.layout.simple_expandable_list_item_1,getStops(agency,route,direction));
-			stopspinner.setAdapter(stopArrayAdapter);
+			stopspinner.setAdapter(stopArrayAdapter);*/
 			//stopspinner.setOnItemSelectedListener(new directionlistener());
+		}
+
+		@Override
+		public void onNothingSelected(AdapterView<?> arg0) {
+			// TODO Auto-generated method stub
+
+		}
+	}
+	
+	public class stoplistener implements OnItemSelectedListener {
+		@Override
+		public void onItemSelected(AdapterView<?> parent, View arg1, int pos,
+				long id) {
+			//stopspinner = (Spinner)findViewById(R.id.stopSpinner);
+			//stop = directionmap.get(parent.getItemAtPosition(pos).toString());
+			stop = parent.getItemAtPosition(pos).toString();
+			
+			/*ArrayAdapter<String> stopArrayAdapter = new ArrayAdapter<String>(MainActivity.this,
+					android.R.layout.simple_expandable_list_item_1,getStops(agency,route,direction));
+			stopspinner.setAdapter(stopArrayAdapter);*/
+			//stopspinner.setOnItemSelectedListener(new directionlistener());
+			final LayoutInflater inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+			LinearLayout linearLayout = (LinearLayout)findViewById(R.id.listOfStops);
+			FrameLayout temp = (FrameLayout) inflater.inflate(R.layout.bus_info_fragment,null);
+			linearLayout.addView(temp);
+			
+			Bus_Stop new_stop = new Bus_Stop(agency,"AC Transit", route, "Hearst Av & Le Roy Av", stop, temp);
+			stops.add(new_stop);
 		}
 
 		@Override
